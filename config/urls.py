35dcd4b6448c0b81003aa60e4a22a61e8d39bb75
makhailya <makhailya@gmail.com>
2026-05-02 16:29:42 +0500

    "feat: add drf-spectacular for Swagger UI, auto-activate users, fix CORS and project settings" (сделано: добавьте drf-эффектный интерфейс для Swagger UI, автоматическую активацию пользователей, исправьте CORS и настройки проекта"

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     # Админка Django
@@ -14,6 +16,8 @@ urlpatterns = [
     # Даёт нам готовые эндпоинты для регистрации и входа
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 # Это нужно чтобы Django отдавал загруженные картинки в режиме разработки
